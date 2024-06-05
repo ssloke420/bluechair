cc98f908de4a77b233550dba21f9c9767dcb01d7
ssloke420 <131798522+ssloke420@users.noreply.github.com>
2024-06-05 15:40:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function dare() {
+  alert("You dare click the blue chair?");
+  alert("I'll show you what happens next!");
+  alert("You will be sent to a new page for your sins!");
+  window.location.href = "https://clicktheredbutton.com/";
+}
+document.addEventListener("keydown", function() {
+  alert("You dare click the blue chair?");
+  alert("I'll show you what happens next!");
+  alert("You will be sent to a new page for your sins!");
+  window.location.href = "https://clicktheredbutton.com/";
+});
